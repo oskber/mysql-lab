aa72df0979eb902c57272d5cd62d1726fe006f56
Oskar Bergman <ocke_bergman@hotmail.com>
2024-10-28 09:41:10 +0100

    refactor view

--- bookstore.sql
+++ bookstore.sql
@@ -14,7 +14,7 @@ CREATE TABLE author
     id         INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
-    birth_date DATE NOT NULL
+    birth_date DATE         NOT NULL
 );
 CREATE TABLE language
 (
@@ -24,11 +24,11 @@ CREATE TABLE language
 CREATE TABLE book
 (
     isbn             VARCHAR(13) PRIMARY KEY,
-    title            VARCHAR(255) NOT NULL,
-    language_id      INT NOT NULL,
+    title            VARCHAR(255)   NOT NULL,
+    language_id      INT            NOT NULL,
     price            DECIMAL(10, 2) NOT NULL,
-    publication_date DATE NOT NULL,
-    author_id        INT NOT NULL,
+    publication_date DATE           NOT NULL,
+    author_id        INT            NOT NULL,
     FOREIGN KEY (author_id) REFERENCES author (id),
     FOREIGN KEY (language_id) REFERENCES language (id)
 );
@@ -40,9 +40,9 @@ CREATE TABLE bookstore
 );
 CREATE TABLE inventory
 (
-    store_id INT NOT NULL,
+    store_id INT         NOT NULL,
     isbn     VARCHAR(13) NOT NULL,
-    amount   INT NOT NULL,
+    amount   INT         NOT NULL,
     PRIMARY KEY (store_id, isbn),
     FOREIGN KEY (store_id) REFERENCES bookstore (id),
     FOREIGN KEY (isbn) REFERENCES book (isbn)
@@ -65,7 +65,7 @@ VALUES ('9780571334650', 'Normal People', 1, 150.00, '2018-08-28', 1),
        ('9781524798628', 'Daisy Jones & The Six', 1, 170.00, '2019-03-05', 3),
        ('9780735219090', 'Where the Crawdads Sing', 1, 180.00, '2018-08-14', 4),
        ('9781786892737', 'The Midnight Library', 1, 190.00, '2020-08-13', 5),
-        ('9781142132132', 'Intermezzo', 2, 200.00, '2024-09-01', 1);
+       ('9781142132132', 'Intermezzo', 2, 200.00, '2024-09-01', 1);
 
 INSERT INTO bookstore(name, city)
 VALUES ('Akademibokhandeln', 'Luleå'),
@@ -73,20 +73,24 @@ VALUES ('Akademibokhandeln', 'Luleå'),
        ('Bokus', 'Göteborg');
 
 INSERT INTO inventory(store_id, isbn, amount)
-VALUES (1, '9780571334650', 100),  -- Normal People
-       (1, '9781501171345', 150),  -- It Ends with Us
-       (2, '9781524798628', 200),  -- Daisy Jones & The Six
-       (2, '9780735219090', 180),  -- Where the Crawdads Sing
-       (3, '9781786892737', 140),  -- The Midnight Library
-        (1, '9781142132132', 100);  -- Intermezzo
+VALUES (1, '9780571334650', 100), -- Normal People
+       (1, '9781501171345', 150), -- It Ends with Us
+       (2, '9781524798628', 200), -- Daisy Jones & The Six
+       (2, '9780735219090', 180), -- Where the Crawdads Sing
+       (3, '9781786892737', 140), -- The Midnight Library
+       (1, '9781142132132', 100); -- Intermezzo
+
+DROP VIEW IF EXISTS total_author_book_value;
 
 CREATE VIEW total_author_book_value AS
-SELECT CONCAT(a.first_name, ' ', a.last_name) AS name,
-       YEAR(CURDATE()) - YEAR(a.birth_date)   AS age,
-       COUNT(b.isbn)                          AS book_title_count,
-       CONCAT(SUM(b.price * i.amount), ' kr') AS inventory_value
-FROM author a
-         JOIN book b ON a.id = b.author_id
-         JOIN inventory i ON b.isbn = i.isbn
-GROUP BY a.id;
+SELECT CONCAT(author.first_name, ' ', author.last_name)  AS name,             -- Concatenate first and last name
+       YEAR(CURDATE()) - YEAR(author.birth_date)         AS age,              -- Calculate age
+       COUNT(book.isbn)                                  AS book_title_count, -- Count the number of books
+       CONCAT(SUM(book.price * inventory.amount), ' kr') AS inventory_value   -- Calculate total inventory value
+FROM author
+         JOIN
+     book ON author.id = book.author_id
+         JOIN
+     inventory ON book.isbn = inventory.isbn
+GROUP BY author.id;
 
